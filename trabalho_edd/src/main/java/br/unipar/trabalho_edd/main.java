b39f89dbe7e1bdd125275aebf349eeb0c6f03d7b
wazlawick <victor.wazlawick@edu.unipar.br>
2023-04-20 20:55:14 -0300

    Feitos os métodos de ordenação por seleção e bolha e feito o switch para decidir qual das ordenação escolher

--- trabalho_edd/src/main/java/br/unipar/trabalho_edd/main.java
+++ trabalho_edd/src/main/java/br/unipar/trabalho_edd/main.java
@@ -2,15 +2,35 @@ package br.unipar.trabalho_edd;
 
 import br.unipar.trabalho_edd.models.Ordenar;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class main {
 
     public static void main(String[] args) {
-        int[]vetor = {90,23,12,43,54,75};
+        ArrayList<Integer> vetor = new ArrayList<>();
         
         Ordenar o1 = new Ordenar();
         
-        o1.ordenacaoInsercao(vetor);
+        String input = JOptionPane.showInputDialog("Qual método de ordenação deseja visualizar: "
+                + " Ordenação por Inserção(I), Ordenação por Seleção(S) ou Ordenação Bolha(B)");
+        
+        
+        
+        
+        switch (input.toUpperCase()) {
+            case "I":
+                o1.ordenacaoInsercao(vetor);
+                break;
+            case "S":
+                o1.ordenacaoSelecao(vetor);
+                break;
+            case "B":
+                o1.ordenacaoBolha(vetor);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opção inválida");
+                break;
+        }
         
         System.out.println(o1.toString());
         
--- trabalho_edd/src/main/java/br/unipar/trabalho_edd/models/Ordenar.java
+++ trabalho_edd/src/main/java/br/unipar/trabalho_edd/models/Ordenar.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Ordenar {
     private ArrayList<Integer> listaNumeros = new ArrayList<>();
-    int jInsercao,chave;
+
     
     public ArrayList<Integer> getListaNumeros() {
         return listaNumeros;
@@ -14,19 +14,61 @@ public class Ordenar {
         this.listaNumeros.add(listaNumeros);
     }
     
-    public void ordenacaoInsercao(int[]vetor){
-        for(int i=0; i<vetor.length;i++){
-            listaNumeros.add(vetor[i]);
-        }
-        for(int i=0; i<listaNumeros.size();i++){
-            chave = listaNumeros.get(i);
-            
-            for(jInsercao = i-1; jInsercao>=0 && listaNumeros.get(jInsercao)>chave;jInsercao--){
-                listaNumeros.set(jInsercao+1, listaNumeros.get(jInsercao));
-            }
-            listaNumeros.set(jInsercao+1, chave);
+    private void inclusaoValores(ArrayList<Integer> vetor){
+        for(int i=0;i<vetor.size();i++){
+            listaNumeros.add(vetor.get(i));
         }
     }
+    
+    public void ordenacaoInsercao(ArrayList<Integer> vetor){
+        int j,chave;
+        
+        inclusaoValores(vetor);
+        
+        for(int i=1; i<listaNumeros.size();i++){
+            chave = listaNumeros.get(i);
+            
+            for(j= i-1; j>=0 && listaNumeros.get(j)>chave;j--){
+                listaNumeros.set(j+1, listaNumeros.get(j));
+            }
+            listaNumeros.set(j+1, chave);
+        }
+    }
+    
+    public void ordenacaoSelecao(ArrayList<Integer> vetor){
+        inclusaoValores(vetor);
+        
+        for(int i=0;i<listaNumeros.size()-1;i++){
+            int posMenor = i;
+            
+            for(int j=i+1;j<listaNumeros.size();j++){
+                if(listaNumeros.get(j)<listaNumeros.get(posMenor)){
+                    posMenor = j;
+                }
+            }
+            if(posMenor!=i){
+                int aux = listaNumeros.get(posMenor);
+                listaNumeros.set(posMenor, listaNumeros.get(i));
+                listaNumeros.set(i, aux);
+            }
+        }
+    }
+    
+    public void ordenacaoBolha(ArrayList<Integer> vetor){
+        inclusaoValores(vetor);
+        
+        for(int i=0; i<listaNumeros.size()-1;i++){
+            for(int j=0; j<listaNumeros.size()-i-1; j++){
+                if(listaNumeros.get(j)>listaNumeros.get(j+1)){
+                    int aux = listaNumeros.get(j);
+                    listaNumeros.set(j, listaNumeros.get(j+1));
+                    listaNumeros.set(j+1, aux);
+                }
+            }
+        }
+    }
+    
+    
 
     @Override
     public String toString() {
