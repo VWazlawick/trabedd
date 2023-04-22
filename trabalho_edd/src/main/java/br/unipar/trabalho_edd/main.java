cb05655ac352bbc5c8948b44e38c8737e62b6582
wazlawick <victor.wazlawick@edu.unipar.br>
2023-04-22 10:17:55 -0300

    Ajustado alguns métodos e feito o método que imprime todas as ordenações, também feito a documentação de como funciona o projeto

--- trabalho_edd/src/main/java/br/unipar/trabalho_edd/main.java
+++ trabalho_edd/src/main/java/br/unipar/trabalho_edd/main.java
@@ -7,17 +7,16 @@ import javax.swing.JOptionPane;
 public class main {
 
     public static void main(String[] args) {
-        int[]vetor = null;
-        String msg ="";
+        int[]vetor = null;   
                 
         Ordenar o1 = new Ordenar();
         
         String input = JOptionPane.showInputDialog("Qual método de ordenação deseja visualizar: "
-                + " Ordenação por Inserção(I), Ordenação por Seleção(S) ou Ordenação Bolha(B)");
+                + " Ordenação por Inserção(I), Ordenação por Seleção(S), Ordenação Bolha(B) ou comparar todos(T)");
+        //Solicitado ao usuário método que deseja visualizar
         
         
-        
-        
+        //Switch vai ler quando o método que o usuário escolheu e rodar os métodos de execução
         switch (input.toUpperCase()) {
             case "I":
                 vetor = o1.inserirVetor(vetor);
@@ -31,16 +30,20 @@ public class main {
                 vetor = o1.inserirVetor(vetor);
                 o1.ordenacaoBolha(vetor);
                 break;
+            case "T":
+                vetor = o1.inserirVetor(vetor);
+                o1.ordenacaoInsercao(vetor);
+                o1.ordenacaoSelecao(vetor);
+                o1.ordenacaoBolha(vetor);
+                break;
             default:
                 JOptionPane.showMessageDialog(null, "Opção inválida");
                 break;
         }
+
         
-        for(int i=0; i<vetor.length;i++){
-            msg += vetor[i] + ",";
-        }
         
-        System.out.println("Primeira lista: {" + msg + "}" +"\nLista Ordenada: " + o1.toString());
+        JOptionPane.showMessageDialog(null, o1.toString());
         
     }
 }
--- trabalho_edd/src/main/java/br/unipar/trabalho_edd/models/Ordenar.java
+++ trabalho_edd/src/main/java/br/unipar/trabalho_edd/models/Ordenar.java
@@ -5,6 +5,11 @@ import javax.swing.JOptionPane;
 
 public class Ordenar {
     private ArrayList<Integer> listaNumeros = new ArrayList<>();
+    private long tempoTotal;
+    private String msg = "";
+    //ArrayList criada para manter o vetor original na main e nesta array ordenar ela
+    //Msg feita para retorno quando usuário seleciona para retornar todos os métodos
+    
 
     
     public ArrayList<Integer> getListaNumeros() {
@@ -14,28 +19,31 @@ public class Ordenar {
     public void setListaNumeros(Integer listaNumeros) {
         this.listaNumeros.add(listaNumeros);
     }
-    
+
+    public long getTempoTotal() {
+        return tempoTotal;
+    }
+
+    public void setTempoTotal(long tempoTotal) {
+        this.tempoTotal = tempoTotal;
+    }
+            
     private void inclusaoValores(int[] vetor){
         for(int i=0;i<vetor.length;i++){
             listaNumeros.add(vetor[i]);
         }
     }
-    
-//    public void inserirVetor(ArrayList<Integer> vetor){
-//        for(int i=0;i<100;i++){
-//            String opcao = JOptionPane.showInputDialog("Para adicionar um número(A) - Para sair(S): ");
-//            
-//            if(opcao.toUpperCase().equals("A")){
-//                String inputValor = JOptionPane.showInputDialog("Informe o valor: ");
-//                int valor = Integer.parseInt(inputValor);
-//                vetor.add(valor);
-//            }else if(opcao.toUpperCase()equals("S")){
-//                i =101;
-//            }
-//        }
-//    }
-    
-    
+    //Método usado para fazer uma copia do vetor original para ser ordenado
+      
+    private String imprimirVetor(int[]vetor){
+        String msgRetorno = "";
+        for(int i=0;i<vetor.length;i++){
+            msgRetorno += vetor[i]+",";
+        }
+        return msgRetorno;
+    }
+    //Faz uma String com os valores que estão no vetor original para impressão 
+        
     public int[] inserirVetor(int[]vetor){
         String inputTam = JOptionPane.showInputDialog("Informe o tamanho do vetor: ");
         int tamanho = Integer.parseInt(inputTam);
@@ -45,13 +53,17 @@ public class Ordenar {
             String inputValor = JOptionPane.showInputDialog("Informe o " + (i+1) + "º número:");
             vetor[i] = Integer.parseInt(inputValor);
         }
-        return vetor;
-    }
-    public void ordenacaoInsercao(int[]vetor){
-        int j,chave;
-        
         inclusaoValores(vetor);
+        return vetor;
         
+    }
+    //Método inserirVetor serve para o usuário inserir o tamanho do vetor selecionado 
+    //e inserir os valores segundo o tamanho escolhido
+    
+    public void ordenacaoInsercao(int[]vetor){
+        int j,chave;           
+        
+        long inicio = System.currentTimeMillis();
         for(int i=1; i<listaNumeros.size();i++){
             chave = listaNumeros.get(i);
             
@@ -60,11 +72,18 @@ public class Ordenar {
             }
             listaNumeros.set(j+1, chave);
         }
+        long fim = System.currentTimeMillis();
+        
+        this.tempoTotal = fim-inicio;
+        
+        msg += "\nORDENAÇÃO POR INSENRÇÃO" + "\nLista Inicial: " + "["+ imprimirVetor(vetor) + "]" + "\nLista Ordenada: " + this.listaNumeros 
+                + "\nTempo execução: " + this.tempoTotal;
     }
+    //Método de ordenação por inserção, tem o calculo do tempo dentro dele e o preenchimento da váriavel msg com 
+    //vetor original, o vetor ordenado e o tempo total
     
     public void ordenacaoSelecao(int[]vetor){
-        inclusaoValores(vetor);
-        
+        long inicio = System.currentTimeMillis();
         for(int i=0;i<listaNumeros.size()-1;i++){
             int posMenor = i;
             
@@ -79,10 +98,17 @@ public class Ordenar {
                 listaNumeros.set(i, aux);
             }
         }
+        long fim = System.currentTimeMillis();
+        this.tempoTotal = fim-inicio;
+        
+        msg += "\nORDENAÇÃO POR SELEÇÃO" + "\nLista Inicial: " + "["+ imprimirVetor(vetor) + "]" + "\nLista Ordenada: " + this.listaNumeros 
+                + "\nTempo execução: " + this.tempoTotal;
     }
+    //Método de ordenação por seleção, tem o calculo do tempo dentro dele e o preenchimento da váriavel msg com 
+    //vetor original, o vetor ordenado e o tempo total
     
-    public void ordenacaoBolha(int[]vetor){
-        inclusaoValores(vetor);
+    public void ordenacaoBolha(int[]vetor){       
+        long inicio = System.currentTimeMillis();
         
         for(int i=0; i<listaNumeros.size()-1;i++){
             for(int j=0; j<listaNumeros.size()-i-1; j++){
@@ -93,14 +119,23 @@ public class Ordenar {
                 }
             }
         }
+        long fim = System.currentTimeMillis();
+        
+        this.tempoTotal = fim-inicio;
+        
+        msg += "\nORDENAÇÃO BOLHA" + "\nLista Inicial: " + "[" + imprimirVetor(vetor) + "]"+ "\nLista Ordenada: " + this.listaNumeros 
+                + "\nTempo execução: " + this.tempoTotal;
     }
+    //Método de ordenação bolha, tem o calculo do tempo dentro dele e o preenchimento da váriavel msg com 
+    //vetor original, o vetor ordenado e o tempo total
     
     
 
     @Override
     public String toString() {
-        return "Ordenar{" + "listaNumeros=" + listaNumeros + '}';
+        return msg;
     }
+    //Feito alteração no toString para retornar a váriavel msg já formatada
     
     
 }
